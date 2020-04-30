1692759676659611cb37dd663de0145f2192167c
corytran <cktran02@gmail.com>
2020-04-29 21:16:53 -0700

    edits

--- socialdistancing/Control.java
+++ socialdistancing/Control.java
@@ -180,19 +180,6 @@ public class Control extends Building {
 		}
 		
 
-		public void personToWallCollision(Person p) {
-			
-			Rectangle personRect = new Rectangle(p.x,p.y, p.width, p.height);
-			for(int i = 0; i < walls.length;i++)
-			{
-				if(r[i].intersects(personRect))
-					if(walls[i].vertical)
-					{
-						p.vx *= -1;
-					}
-					else
-						p.vy *= -1;
-			}
-		}
+	
 		
 }
